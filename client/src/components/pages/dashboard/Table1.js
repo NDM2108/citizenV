1856377888254944bcf7d1e559f990eff267948b
NDM2108 <63856585+NDM2108@users.noreply.github.com>
2021-12-22 01:34:01 +0700

    table1

--- client/src/components/pages/dashboard/Table1.js
+++ client/src/components/pages/dashboard/Table1.js
@@ -8,123 +8,37 @@ import { visuallyHidden } from '@mui/utils';
 import { useEffect, useState } from "react"
 import axios from 'axios';
 
-function BangTienDo() {
-    const [province, setProvince] = useState([])
-    useEffect(() => {
-        axios.get('http://localhost:5000/provinces')
-            .then(response => {
-                const province = response.data
-                setProvince(province)
-            })
-    }, [])
-    const keys = (province)
-    console.log(keys)
+var keys
 
-    const [details, setDetails] = useState([])
-    
-    const getBadge = (status) => {
-        switch (status) {
-            case 'Active':
-                return 'success'
-            case 'Inactive':
-                return 'secondary'
-            case 'Pending':
-                return 'warning'
-            case 'Banned':
-                return 'danger'
-            default:
-                return 'primary'
-        }
-    }
-    const toggleDetails = (index) => {
-        const position = details.indexOf(index)
-        let newDetails = details.slice()
-        if (position !== -1) {
-            newDetails.splice(position, 1)
-        } else {
-            newDetails = [...details, index]
-        }
-        setDetails(newDetails)
-    }
+function BangTienDo() {
+    // const [province, setProvince] = useState([])
+    // useEffect(() => {
+    //     axios.get('http://localhost:5000/provinces')
+    //         .then(response => {
+    //             const province = response.data
+    //             setProvince(province)
+    //         })
+    // }, [])
+    // const keys = (province)
+    // console.log(keys)
+
+    axios.get('http://localhost:5000/provinces')
+            .then(response => {
+                keys = response.data
+            })
+    console.log(keys);
     
     return (
-        <div >
-            <br></br>
-            <br></br>
-            <CSmartTable
-                activePage={3}
-                cleaner
-                clickableRows
-                // columns={columns}
-                columnFilter
-                columnSorter
-                items={keys}
-                itemsPerPageSelect
-                itemsPerPage={5}
-                pagination
-                scopedColumns={{
-                    status: (item) => (
-                        <td>
-                            <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
-                        </td>
-                    ),
-                    show_details: (item) => {
-                        return (
-                            <td className="py-2">
-                                <CButton
-                                    color="primary"
-                                    variant="outline"
-                                    shape="square"
-                                    size="sm"
-                                    onClick={() => {
-                                        toggleDetails(item._id)
-                                    }}
-                                >
-                                    {details.includes(item._id) ? 'Hide' : 'Show'}
-                                </CButton>
-                            </td>
-                        )
-                    },
-                    details: (item) => {
-                        return (
-                            <CCollapse visible={details.includes(item._id)}>
-                                <CCardBody>
-                                    <h4>{item.username}</h4>
-                                    <p className="text-muted">User since: {item.registered}</p>
-                                    <CButton size="sm" color="info">
-                                        User Settings
-                                    </CButton>
-                                    <CButton size="sm" color="danger" className="ml-1">
-                                        Delete
-                                    </CButton>
-                                </CCardBody>
-                            </CCollapse>
-                        )
-                    },
-                }}
-                selectable
-                sorterValue={{ column: 'name', state: 'asc' }}
-                tableFilter
-                tableHeadProps={{
-                    color: 'danger',
-                }}
-                tableProps={{
-                    striped: true,
-                    hover: true,
-                }}
-            />
-            {/* <CSmartTable
-                items={province}
-                columnFilter
-                columnSorter
-                pagination
-                tableProps={{
-                    hover: true,
-                }}
-            /> */}
-        </div >
-
-    )
+        <CSmartTable
+          items={keys}
+          columnFilter
+          columnSorter
+          pagination
+          tableProps={{
+            hover: true,
+          }}
+        />
+      )
 }
 
 
